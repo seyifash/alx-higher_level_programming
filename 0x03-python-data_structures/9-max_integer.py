5bf2583f4546b13384f70a7b416a1c4bfc09b81e
seyifash <fasugbeoluwaseyi@gmail.com>
2023-06-11 11:23:03 -0700

    maximum number

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+    max_n = my_list[0]
+    for i in my_list:
+        if i > max_n:
+            max_n = i
+    return (max_n)
