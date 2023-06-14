309638c4ff849bccc792044389301a5948ce7782
seyifash <fasugbeoluwaseyi@gmail.com>
2023-06-14 11:35:41 -0700

    updats the value of a key in a dict

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,4 +2,4 @@
 def print_sorted_dictionary(a_dictionary):
     sorted_items = sorted(a_dictionary)
     for key in sorted_items:
-        print(key + ": " + str(a_dictionary[key]))
+        print("{}: {}".format(key, a_dictionary[key]))
