4939d629537064487cfd8d93a8a37bb8ebf66751
seyifash <fasugbeoluwaseyi@gmail.com>
2023-06-14 08:09:05 -0700

    adds all unique num in a list

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
     uniq_num = set(my_list)
-    sun_uniq = 0
+    sum_uniq = 0
     for num in uniq_num:
         sum_uniq += num
     return sum_uniq
