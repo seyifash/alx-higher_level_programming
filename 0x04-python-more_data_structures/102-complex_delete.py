6adc099100fc74596afab0ceb4e6b21973b4d261
seyifash <fasugbeoluwaseyi@gmail.com>
2023-06-14 19:22:52 -0700

    deletes key and it value based on the given value

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    listofkeys = list(a_dictionary.keys())
-
-    for values in listofkeys:
-        if value == a_dictionary.get(values):
-            del a_dictionary(values)
-        return a_dictionary
+    delkeys = [keys for keys, val in a_dictionary.items() if value == val]
+    for keys in delkeys:
+        del a_dictionary[keys]
+    return a_dictionary
