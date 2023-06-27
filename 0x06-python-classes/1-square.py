eaf6c8073484646cbec04950f37ec5c6beff8ab2
seyifash <fasugbeoluwaseyi@gmail.com>
2023-06-27 11:12:19 -0700

    initializes a private intsance atribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines a class square"""
+
+
+class Square:
+    """Defines a square"""
+
+    def __init__(self, size):
+        """initializes a private instance attribute
+
+        Args:
+        size: the size of  a new square
+        """
+        self.__size = size
