1e6c89154682bf7ef6c26985c7c945e9e5ba1a1b
seyifash <fasugbeoluwaseyi@gmail.com>
2023-07-02 12:02:03 -0700

    python doctest

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -13,7 +13,7 @@ def print_square(size):
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
-    for i in range(0, size):
-        for j in range(0, size):
+    for i in range(size):
+        for j in range(size):
             print("#", end="")
-            print()
+        print()
