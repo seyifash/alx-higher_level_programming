dbacc444d6fa20139364e8959b10021749b2c7ea
seyifash <fasugbeoluwaseyi@gmail.com>
2023-07-03 11:11:59 -0700

    rectangle representation

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -58,9 +58,7 @@ class Rectangle:
 
         rect = []
         for i in range(self.__height):
-           [rect.append('#') for j in range(self.__width)]
-           if i != self.__height - 1:
+            [rect.append('#') for j in range(self.__width)]
+            if i != self.__height - 1:
                 rect.append("\n")
         return "".join(rect)
-
-
