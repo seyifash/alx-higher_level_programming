a2d20778f8e0c8e45e547fc0f0e4ddf413c6e747
seyifash <fasugbeoluwaseyi@gmail.com>
2023-07-11 06:54:06 -0700

    reading from the file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,6 +3,6 @@
 
 
 def read_file(filename=""):
-    """reads a text file (UTF-8) and prints to stdout"""
+    """reads a text file (UTF-8) and prints to stdout."""
     with open(filename, encoding="utf-8") as f:
-        print(f.read(). end="")
+        print(f.read(), end="")
