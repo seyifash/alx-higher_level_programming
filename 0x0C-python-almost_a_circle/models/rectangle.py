5ccc51b3480fd9ff809718309ccf039e0706c5e9
seyifash <fasugbeoluwaseyi@gmail.com>
2023-07-17 22:34:59 -0700

    code fixed

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -4,10 +4,22 @@ from models.base import Base
 
 
 class Rectangle(Base):
-    """A class that inherits from the base class"""
+    """A class that represents a rectangle"""
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        """initialize the objects for the rectangle class"""
+        """initialize the objects for the rectangle class
+        Args:
+            width (int): The width of the new Rectangle.
+            height (int): The height of the new Rectangle.
+            x (int): The x coordinate of the new Rectangle.
+            y (int): The y coordinate of the new Rectangle.
+            id (int): The identity of the new Rectangle.
+        Raises:
+            TypeError: If either of width or height is not an int.
+            ValueError: If either of width or height <= 0.
+            TypeError: If either of x or y is not an int.
+            ValueError: If either of x or y < 0.
+        """
         super().__init__(id)
         self.width = width
         self.height = height
