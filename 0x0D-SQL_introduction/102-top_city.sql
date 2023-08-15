b79f507de77fe6f6a8167473e9fd1f9f3c4341f9
seyifash <fasugbeoluwaseyi@gmail.com>
2023-08-15 12:17:57 -0700

    displays between a an atribute value

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -2,7 +2,7 @@
 -- ordered by temperature (descending)
 SELECT city, AVG(value) AS avg_temp
 FROM temperatures
-WHERE MONTH(date) BETWEEN 7 AND 8
+WHERE month BETWEEN 7 AND 8
 GROUP BY city
 ORDER BY avg_temp DESC
 LIMIT 3;
