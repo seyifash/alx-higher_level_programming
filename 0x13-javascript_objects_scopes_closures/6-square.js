a22862282d0a841410b189807487132f0426bc7d
seyifash <fasugbeoluwaseyi@gmail.com>
2023-09-12 12:47:45 -0700

    inheritance

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const BaseSquare = require('./5-square');
+
+class Square extends BaseSquare {
+  charPrint (c) {
+    for (let i = 0; i < this.height; i++) {
+      if (c === undefined) {
+        console.log('X'.repeat(this.width));
+      } else {
+        console.log(c.repeat(this.width));
+      }
+    }
+  }
+}
+
+module.exports = Square;
