8e9e36cce4f64357d8e9d93c35de06120af44033
seyifash <fasugbeoluwaseyi@gmail.com>
2023-09-12 14:38:58 -0700

    dict

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const { dict } = require('./101-data');
+
+const userOccurrences = {};
+
+for (const userId in dict) {
+  const occurrences = dict[userId];
+
+  if (!userOccurrences[occurrences]) {
+    userOccurrences[occurrences] = [];
+  }
+  userOccurrences[occurrences].push(parseInt(userId));
+}
+console.log(userOccurrences);
