f8d8e24145eb063a1bd4a22b66980cbc10b48942
seyifash <fasugbeoluwaseyi@gmail.com>
2023-09-15 02:26:44 -0700

    fix code

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -14,26 +14,26 @@ if __name__ == "__main__":
     database = sys.argv[3]
     state_name = sys.argv[4]
 
-db = MySQLdb.connect(
-         host="localhost",
-         port=3306,
-         user=username,
-         passwd=password,
-         db=database
-         )
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            passwd=password,
+            db=database
+            )
 
-mycursor = db.cursor()
-sql_query = '''
-SELECT cities.id, cities.name
-FROM cities INNER JOIN states ON cities.state_id = states.id
-WHERE states.name = %s
-ORDER BY cities.id ASC
-'''
-mycursor.execute(sql_query, (state_name,))
-cities = [row[1] for row in mycursor.fetchall()]
+    mycursor = db.cursor()
+    sql_query = '''
+    SELECT cities.id, cities.name
+    FROM cities INNER JOIN states ON cities.state_id = states.id
+    WHERE states.name = %s
+    ORDER BY cities.id ASC
+    '''
+    mycursor.execute(sql_query, (state_name,))
+    cities = [row[1] for row in mycursor.fetchall()]
 
-mycursor.close()
-db.close()
+    mycursor.close()
+    db.close()
 
-city_names = ", ".join(cities)
-print(city_names)
+    city_names = ", ".join(cities)
+    print(city_names)
