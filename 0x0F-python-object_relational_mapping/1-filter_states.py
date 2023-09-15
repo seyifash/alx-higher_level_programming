27b5b4a148632c5d19d2366f1528a5ca5ae07185
seyifash <fasugbeoluwaseyi@gmail.com>
2023-09-15 07:08:16 -0700

    fix code

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -22,7 +22,7 @@ if __name__ == "__main__":
     mycursor.execute("""
             SELECT *
             FROM states
-            WHERE name LIKE 'N%'
+            WHERE name LIKE BINARY 'N%'
             ORDER BY id ASC
         """)
     states = mycursor.fetchall()
