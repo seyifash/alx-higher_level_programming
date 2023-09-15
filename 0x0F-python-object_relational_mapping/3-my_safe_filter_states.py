eddd3a782b1e7536cbec5b471745cb1b02d0f07a
seyifash <fasugbeoluwaseyi@gmail.com>
2023-09-15 02:43:56 -0700

    fix code

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""script that takes in arguments and displays all values in the states table
+and prevents injection
+"""
 import MySQLdb
 import sys
 
@@ -9,21 +12,21 @@ if __name__ == "__main__":
     database = sys.argv[3]
     state_name = sys.argv[4]
 
-db = MySQLdb.connect(
-         host="localhost",
-         port=3306,
-         user=username,
-         passwd=password,
-         db=database
-         )
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            passwd=password,
+            db=database
+            )
 
-mycursor = db.cursor()
-sql_query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
-mycursor.execute(sql_query, (state_name,))
-states = mycursor.fetchall()
+    mycursor = db.cursor()
+    sql_query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+    mycursor.execute(sql_query, (state_name,))
+    states = mycursor.fetchall()
 
-for state in states:
-    print(state)
+    for state in states:
+        print(state)
 
-mycursor.close()
-db.close()
+    mycursor.close()
+    db.close()
