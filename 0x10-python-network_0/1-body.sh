a5ef6eca21086fe27dd6915b91f3feddd1679acc
seyifash <fasugbeoluwaseyi@gmail.com>
2023-09-28 16:11:55 +0100

    display only if status code is 200

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 # script that takes in a URL, sends a GET request to the URL, and displays the body of the response
-# Display only body of a 200 status code response
 
-status_code=$(curl -Ls -X HEAD "$1" | grep -i "HTTP" | cut -d " " -f 2)
-if [[ "$status_code" -eq 200 ]];
-then
-	curl -Ls "$1"
-fi	
+if [ $(curl -L -s -X HEAD -w "%{http_code}" "$1") == '200' ]; then curl -Ls "$1"; fi	
