cb97f03c8ee13dd9f41c06301c5f0b3eb878f360
seyifash <fasugbeoluwaseyi@gmail.com>
2023-09-28 12:38:53 -0700

    peak element in the list

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that takes in a URL and displays all HTTP methods the server will accept
-curl -s -I -X OPTIONS "$1" | grep 'Allow:' | cut -f2- -d' '
+curl -s -I -X OPTIONS "$1" | grep "Allow:" | cut -f 2- -d" "
