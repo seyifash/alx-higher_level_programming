4b97f00a7fad60632b71ae92d6c051dfe46399c9
seyifash <fasugbeoluwaseyi@gmail.com>
2023-09-28 11:31:49 -0700

    all done

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""script for finding peak in list of ints, interview prep
+"""
+
+
+def find_peak(list_of_integers):
+    if not list_of_integers:
+        return None
+
+    left, right = 0, len(list_of_integers) - 1
+
+    while left < right:
+        mid = (left + right) // 2
+
+        if list_of_integers[mid] > list_of_integers[mid + 1]:
+            right = mid
+        else:
+            right = mid
+    return list_of_integers[left]
