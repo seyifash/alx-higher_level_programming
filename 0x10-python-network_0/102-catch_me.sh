7bc9b41e8ab50faf4d0d3f1d2726380040c924f8
seyifash <fasugbeoluwaseyi@gmail.com>
2023-09-28 12:29:49 -0700

    put method

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message
-curl -s -X PUT -d "user_id=98" 0.0.0.0:5000/catch_me  -H "Origin:School
+curl -s -L -X PUT -d "user_id=98" -H "Origin: HolbertonSchool" 0.0.0.0:5000/catch_me
