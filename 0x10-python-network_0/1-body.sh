40ee0c1522d95728e49d5d49e667155a384b892c
seyifash <fasugbeoluwaseyi@gmail.com>
2023-09-28 16:09:23 +0100

    display only if status code is 200

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/bin/bash
 # script that takes in a URL, sends a GET request to the URL, and displays the body of the response
 # Display only body of a 200 status code response
 
