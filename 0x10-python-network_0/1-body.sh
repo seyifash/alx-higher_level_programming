96f7e7bf529171ff7cf3ca0f785e5f16c8906cbf
seyifash <fasugbeoluwaseyi@gmail.com>
2023-09-28 08:37:01 -0700

    displays a get response ony if status is 200

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 # script that takes in a URL, sends a GET request to the URL
 
-status_code=$(curl -L -s -X HEAD "$1" | grep -i "HTTP" | cut -d " " -f 2)
+stat_code="$(curl -L -s -X HEAD "$1" | grep -i "HTTP" | cut -d " " -f 2)"
 
-if [ "$status_code" -eq 200 ];
-then
-	curl -Ls "$1"
-fi
+if [ "$stat_code" -eq 200 ]; then curl -Ls "$1"; fi
