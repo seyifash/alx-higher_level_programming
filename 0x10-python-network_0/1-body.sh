121e4071ef17a0b82946d4d333a7ed46c48ffa29
root <root@DESKTOP-OKAL0M0>
2023-09-28 15:54:29 +0100

    display only if status code is 200

---
+++ 0x10-python-network_0/1-body.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+# script that takes in a URL, sends a GET request to the URL, and displays the body of the response
+# Display only body of a 200 status code response
+
+status_code=$(curl -Ls -X HEAD "$1" | grep -i "HTTP" | cut -d " " -f 2)
+if [[ "$status_code" -eq 200 ]];
+then
+	curl -Ls "$1"
+fi	
