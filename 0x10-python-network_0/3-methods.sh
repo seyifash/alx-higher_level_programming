23598a86fdb22628dbf3c86d82f25fdfdaed90bd
seyifash <fasugbeoluwaseyi@gmail.com>
2023-09-28 08:57:20 -0700

    dsplays hhtp methods accepted by the server

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Bash script that takes in a URL and displays all HTTP methods the server will accept.
-
-curl -s -I -X OPTIONS "$1" | grep -i "Allow:" | cut -d " " -f 2-
+# Bash script that takes in a URL and displays all HTTP methods the server will accept
+curl -s -I -X OPTIONS "$1" | grep 'Allow:' | cut -f2- -d' '
