fb963f6a2954d9a280bc50001e052acab72b2ddd
seyifash <fasugbeoluwaseyi@gmail.com>
2023-10-24 11:55:53 -0700

    the fix

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,9 +4,9 @@ request(process.argv[2], (error, response, body) => {
   if (error) {
     console.error(error);
   } else {
-    const moviesData = JSON.parse(body);
+    const moviesDat = JSON.parse(body);
     let count = 0;
-    moviesData.results.forEach((movie) => {
+    moviesDat.results.forEach((movie) => {
       if (movie.characters.includes('https://swapi-api.alx-tools.com/api/people/18/')) {
         count++;
       }
